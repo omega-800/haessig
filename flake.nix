e59b0c0ca0a24ba18192c145fc9e75805b9a61ed
omega-800 <georgiy.shevoroshkin@ost.ch>
2026-04-24 10:10:55 +0200

    update flake.{nix,lock}

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "A Nix-flake-based Rust development environment";
+  description = "rust development environment";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,56 +16,85 @@
       rust-overlay,
     }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems f;
+      systems = nixpkgs.lib.platforms.unix;
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+              config = { };
+              overlays = [
+                rust-overlay.overlays.default
+                self.overlays.default
+              ];
+            }
+          )
+        );
+      pname = "haessig";
     in
     {
-      overlays.default = final: prev: {
+      overlays.default = _: prev: {
         rustToolchain = prev.rust-bin.stable.latest.default.override {
           extensions = [
             "rust-src"
-            "rustfmt"
             "rustfmt"
           ];
         };
       };
 
-      devShells = forEachSupportedSystem (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-              self.overlays.default
-            ];
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            rust-analyzer
+            fasm
+            gcc
+          ];
+
+          env = {
+            RUST_BACKTRACE = 1;
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
           };
+        };
+      });
+
+      packages = eachSystem (
+        pkgs:
+        let
+          fs = pkgs.lib.fileset;
+          root = ./.;
         in
         {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustToolchain
-              openssl
-              pkg-config
-              cargo-deny
-              cargo-edit
-              cargo-watch
-              rust-analyzer
-              fasm
-            ];
-
-            env = {
-              RUST_BACKTRACE = 1;
-              # Required by rust-analyzer
-              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          default = pkgs.rustPlatform.buildRustPackage {
+            inherit pname;
+            version = "0.0.1";
+            src = fs.toSource {
+              inherit root;
+              fileset = fs.intersection (fs.gitTracked root) (
+                fs.unions [
+                  ./Cargo.toml
+                  ./Cargo.lock
+                  (fs.fileFilter (f: f.hasExt "rs") ./src)
+                ]
+              );
             };
+            cargoLock.lockFile = ./Cargo.lock;
           };
         }
+      );
+      apps = eachSystem (
+        pkgs:
+        pkgs.lib.mapAttrs (_: drv: {
+          type = "app";
+          program = "${drv}${drv.passthru.exePath or "/bin/${drv.pname or drv.name}"}";
+        }) self.packages.${pkgs.system}
       );
     };
 }
